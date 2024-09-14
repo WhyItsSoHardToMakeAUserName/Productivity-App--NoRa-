71dc8b94004863078884df6267a22180c8e324e8
WhyItsSoHardToMakeAUserName <86366420+WhyItsSoHardToMakeAUserName@users.noreply.github.com>
2024-09-14 13:41:30 +0500

    Set up basic routing , and added mock doughnut chart for finance tracker

---
+++ client-side/src/app/feature/finance-tracker/page.tsx
@@ -0,0 +1,49 @@
+'use client'
+import { Doughnut } from "react-chartjs-2";
+
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+
+// Register the components
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+export default function Page(){
+    const data = {
+        labels: ['Red', 'Blue', 'Yellow'],
+        datasets: [
+          {
+            label: '',
+            data: [12, 19, 3],
+            backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)'],
+            borderColor: ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)'],
+            borderWidth: 1,
+          },
+        ],
+        
+      };
+
+      const options = {
+        plugins: {
+          tooltip: {
+            enabled: true,
+            callbacks: {
+              label: function (tooltipItem: { raw: any; }) {
+                // Customize the label text shown in the tooltip
+                return `${tooltipItem.raw} $`;
+              },
+            },
+            displayColors: false, // This removes the color box next to the tooltip label
+          },
+        },
+      };
+      
+    return(
+        <div className="flex items-center justify-center w-1/3">
+            <div className="h-3/4  w-full  flex justify-center">
+                <Doughnut 
+                    data={data}
+                    options={options}
+                />
+            </div>
+        </div>
+    );
+}
--- client-side/src/app/layout.tsx
+++ client-side/src/app/layout.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
 export default function RootLayout({children,}: Readonly<{children: React.ReactNode;}>) {
   return (
     <html lang="en">
-      <body className={''}>
+      <body className='max-h-screen overflow-hidden'>
         {children}
       </body>
     </html>
