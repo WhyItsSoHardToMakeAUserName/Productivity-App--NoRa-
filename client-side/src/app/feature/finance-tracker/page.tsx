1bfe3d3bc6309eab2f7b277e065f2b28669ddc8f
WhyItsSoHardToMakeAUserName <fox125j83773@gmail.com>
2024-09-28 23:18:06 +0500

    minor error fix

--- client-side/src/app/feature/finance-tracker/page.tsx
+++ client-side/src/app/feature/finance-tracker/page.tsx
@@ -1,6 +1,5 @@
 import { FetchFinanceData } from "@/api/FinanceTrackerAPI";
 import FinanceChart from "@/components/FinanceChart";
-import { useSearchParams } from "next/navigation";
 
 export default async function Page(){
   // const [test ,setTest] = useState("")
@@ -11,7 +10,6 @@ export default async function Page(){
   //   .then(data=> setTest(data))
   // },[])
     var data = await FetchFinanceData();
-    const searchParams = useSearchParams();
     return(
         <div className="flex flex-col items-center justify-center w-1/3">
             <div className="h-3/4  w-full  flex justify-center">
